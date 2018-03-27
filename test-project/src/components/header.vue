cfa0512b844e48d4132fe703292d5f467f1c75e3
Aleksey <aleksey.selyavkin@gmail.com>
2018-03-27 23:40:12 +0300

    header start

--- test-project/src/components/header.vue
+++ test-project/src/components/header.vue
@@ -1,7 +1,15 @@
 <template>
-  <div id="header">
-    {{ headerTest }}
-  </div>
+  <header id="header" class="header">
+    <div class="header-wrapper">
+      <div class="logo"><img src="../assets/logo.png" alt=""></div>
+      <div class="search">
+        <input type="search" placeholder="Поиск">
+      </div>
+      <div class="date" id="date">
+        
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -13,8 +21,36 @@
       }
     }
   }
+  function getDate(){
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if(seconds < 10){
+        seconds = '0' + seconds;
+    }
+    document.getElementById('date').innerHTML = hours + ':' + minutes + ':' + seconds;
+  }
+  setInterval(getDate, 0);
+
 </script>
 
 <style lang="scss">
-
+  .header {
+    height: 70px;
+    box-shadow: 0 10px 10px -7px rgba(0,0,0,.5);
+    &-wrapper {
+      margin-left: 250px;
+      display: flex;
+      justify-content: space-between;
+      .logo {
+      width: 50px;
+      img {
+        width: 100%;
+        height: auto;
+        margin-top: 10px;
+      }
+    }
+    }
+  }
 </style>
